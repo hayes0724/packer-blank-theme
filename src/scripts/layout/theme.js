789a7b51464e65f405c136e3af2fc282c4c5406d
Eric Hayes <eric@hayesmarketing.io>
2020-09-28 14:01:40 -0500

    entrypoint templates

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -1,2 +1,15 @@
+/**
+ * Theme Layout Script
+ * ------------------------------------------------------------------------------
+ * A file that contains scripts and styles for all pages.
+ *
+ * @namespace theme
+ * @file vendors@layout.theme.css.liquid
+ * @file vendors@layout.theme.js
+ * @file layout.theme.css.liquid
+ * @file layout.theme.js
+ *
+ */
+
 import "../../styles/theme.scss";
 import "../../styles/theme.scss.liquid";
