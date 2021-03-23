72b896ec76ef5c9d667929e56c17a87c1fda4372
hayes <admin@hayesmarketingfirm.com>
2021-03-23 14:24:08 -0500

    feat: updated packer config defaults to include reload delay
    Signed-off-by: hayes <admin@hayesmarketingfirm.com>

--- packer.config.js
+++ packer.config.js
@@ -1,4 +1,7 @@
 // Override default packer settings and config using this file
 module.exports = {
-
+  // Add a reload delay before refreshing browser in ms, Shopify may need several seconds
+  'network.reload': 0,
+  // Add additional entrypoint
+  'entrypoints': {},
 }
